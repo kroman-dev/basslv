5fb3eea3e14aec49b4e697789f386716001d6f1a
kroman-dev <roman110797@gmail.com>
2025-09-02 22:22:15 +0300

    Add examples with Heston

--- examples/sampleBassMarketHestonSpeedUp.py
+++ examples/sampleBassMarketHestonSpeedUp.py
@@ -1,17 +1,21 @@
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 
-from basslv import BassLocalVolatility, VanillaCall, MarketMarginal, HestonPricingEngine, VisualVerification
+from basslv import BassLocalVolatility, VanillaCall, MarketMarginal
+from basslv import HestonPricingEngine, VisualVerification
+
+from basslv.core.fixedPointEquationDecorator import FixedPointEquationDecorator
+from basslv.visualVerification.visualVerification import _MultiPlotInput
 
 
 if __name__ == '__main__':
+
     startTime = time.time()
 
     sigma = 0.2
-    tenors = [1., 2.]
-    pathsNumber = 50000
     timeGridPoints = 3
+    tenors = np.array([0., 1., 2.])
+    pathsNumber = 100000
     SEED = 42
 
     forward = 1.
@@ -19,9 +23,7 @@ if __name__ == '__main__':
     strikes = np.linspace(0.25, 3., 200, endpoint=True)
     volatility = 0.2
 
-    # t = np.array(tenors)
-    t = np.linspace(0, tenors[-1], timeGridPoints, endpoint=True)
-    # t = np.array([1., 2.])
+    # t = np.linspace(0, tenors[-1], timeGridPoints, endpoint=True)
 
     calls = [
         VanillaCall(
@@ -56,8 +58,11 @@ if __name__ == '__main__':
         for tenorIndex in range(len(tenors))
     ]
 
-    pathsBassLv = BassLocalVolatility().sample(
-        t=t,
+    bassLocalVolatility = BassLocalVolatility()
+    bassLocalVolatility.setFixedPointEquation(FixedPointEquationDecorator())
+
+    pathsBassLv = bassLocalVolatility.sample(
+        t=tenors,
         pathsNumber=pathsNumber,
         marginals=marginals,
         randomGenerator=np.random.default_rng(SEED),
@@ -67,33 +72,33 @@ if __name__ == '__main__':
 
     bassCallPrices = np.mean(np.maximum(pathsBassLv[:, 1][None] - strikes[None].T, 0), 1)
     absError = np.abs(bassCallPrices - np.array(callPrices[0]))
+    relativeError = np.abs(bassCallPrices - np.array(callPrices[0])) / np.array(callPrices[0])
 
     print(time.time() - startTime)
 
-    exit()
-
-    VisualVerification.plot(
-        x=strikes,
-        funcsValues=[absError],
-        labels=['abs error'],
-        title='abs error: bass vs exact (heston)'
+    VisualVerification._multiPlot(
+        [
+            _MultiPlotInput(
+                plot={
+                    "args": [
+                        [strikes],
+                        [relativeError]
+                    ]
+                },
+                set_title={
+                    "args": ["relative error"]
+                }
+            ),
+            _MultiPlotInput(
+                plot={
+                    "args": [
+                        [strikes],
+                        [absError]
+                    ]
+                },
+                set_title={
+                    "args": ["abs error"]
+                }
+            ),
+        ]
     )
-
-    exit()
-
-    _, axis = plt.subplots(figsize=(15, 5), dpi=200)
-
-    handleGroundTruth, *_ = axis.plot(t, pathsGroundTruth.T, c="C0")
-    handleBassLv, *_ = axis.plot(t, pathsBassLv.T, ls=":", c="C1")
-    for tenor in tenors:
-        handleTime = axis.axvline(tenor, c="C2")
-
-    axis.set_xlabel("$t$")
-    axis.set_ylabel("$S_t$")
-    axis.set_title("Path by Bass LV model fitted to syntatic (flat vola) Market marginals")
-    axis.legend(
-        handles=[handleGroundTruth, handleBassLv, handleTime],
-        labels=["Ground Truth", "Bass LV", "Marginals"],
-    )
-
-    plt.show()
